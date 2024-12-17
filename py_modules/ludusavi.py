ed1dc05cdcba8bc864112799f9856e18a6a99548
J <goerkeje@gmail.com>
2024-12-17 12:37:29 -0600

    added flatpak fixes and additional logging and catching all exceptions.

--- py_modules/ludusavi.py
+++ py_modules/ludusavi.py
@@ -10,14 +10,25 @@ class Ludusavi:
     version: str = None
 
     initialized: bool = False
+    Env: dict = None
 
     def __init__(self, bin_paths: list[str]):
+        self.SetEnv
         for path in bin_paths:
             if self._check_initialized(path):
                 break
         self.initialized = True
-
-
+  
+        
+    def SetEnv(self):
+        #flatpak env variables
+        self.Env = os.environ.copy
+        if 'XDG_RUNTIME_DIR' not in self.Env:
+            self.Env['XDG_RUNTIME_DIR'] = '/run/user/1000'
+        if 'WAYLAND_DISPLAY' not in self.Env:
+            self.Env['WAYLAND_DISPLAY'] = 'gamescope-0'
+        if 'LIBRARY_PATH' not in self.Env:
+            self.Env['LIBRARY_PATH'] = '/usr/lib/x86_64-linux-gnu'
     def check_game(self, game_name: str):
         try:
             # Find if the game 'game_name' has support for backup.
@@ -27,7 +38,7 @@ class Ludusavi:
             output = subprocess.check_output(cmd, stderr=subprocess.STDOUT, text=True)
             data = json.loads(output)
             return data["games"][game_name] is not None
-        except subprocess.CalledProcessError as e:
+        except Exception as e:
             output = e.output
             return False
         finally:
@@ -39,7 +50,8 @@ class Ludusavi:
 
         try:
             output = subprocess.check_output(cmd, stderr=subprocess.STDOUT, text=True)
-        except subprocess.CalledProcessError as e:
+        #except subprocess.CalledProcessError as e:
+        except Exception as e:
             output = e.output
         finally:
             decky_plugin.logger.debug(output)
@@ -48,12 +60,7 @@ class Ludusavi:
         cmd = [self.bin_path, 'backup', '--api', '--force', game_name]
         decky_plugin.logger.info("Running command: %s", subprocess.list2cmdline(cmd))
 
-        # Workaround for flatpacks
-        l_env = os.environ.copy()
-        if 'XDG_RUNTIME_DIR' not in l_env:
-            l_env['XDG_RUNTIME_DIR'] = '/run/user/1000'
-
-        process = await asyncio.create_subprocess_exec(*cmd, env=l_env, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
+        process = await asyncio.create_subprocess_exec(*cmd, env=self.Env, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
         stdout, _ = await process.communicate()
 
         result = stdout.decode()
@@ -64,14 +71,16 @@ class Ludusavi:
 
     def _check_initialized(self, bin_path: str) -> bool:
         try:
-            decky_plugin.logger.debug("Trying binary: %s", bin_path)
             
-            output = subprocess.check_output([bin_path, '--version'], stderr=subprocess.STDOUT, text=True)
+            decky_plugin.logger.debug("Trying binary: %s", bin_path)     
+            output = subprocess.check_output([bin_path, '--version'],env=self.Env , stderr=subprocess.STDOUT, text=True)
             decky_plugin.logger.info("Using binary: %s", bin_path)
+            decky_plugin.logger.debug("Command Output:  %s",output) 
             
             self.bin_path = bin_path
             self.version = output
             
             return True
-        except FileNotFoundError:
+        except Exception as e:
+            decky_plugin.logger.error(e)
             return False
