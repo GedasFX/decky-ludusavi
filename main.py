52c7b3a40a69576db4d0d43a7c6799d8e19b9177
GedasFX <contact@gedas.dev>
2025-03-15 17:08:58 +0200

    backup works

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from ludusavi import Ludusavi
 
 decky.logger.setLevel("DEBUG")
 
+
 class Plugin:
     ludusavi: Ludusavi
 
@@ -12,8 +13,8 @@ class Plugin:
     async def get_ludusavi_version(self):
         decky.logger.debug("Executing: get_ludusavi_version()")
         decky.logger.info("bin_path: %s", self.ludusavi.bin_path)
-        return { "bin_path": self.ludusavi.bin_path, "version": self.ludusavi.version }
-    
+        return {"bin_path": self.ludusavi.bin_path, "version": self.ludusavi.version}
+
     async def get_config(self, key: str):
         decky.logger.debug("Executing: get_config('%s')", key)
         return config.app_config.getSetting(key)
@@ -21,7 +22,7 @@ class Plugin:
     async def set_config(self, key: str, value):
         decky.logger.debug("Executing: set_config('%s', '%s')", key, value)
         return config.app_config.setSetting(key, value)
-    
+
     async def get_game_config(self, key: str):
         decky.logger.debug("Executing: get_game_config('%s')", key)
         return config.game_config.getSetting(key)
@@ -29,19 +30,16 @@ class Plugin:
     async def set_game_config(self, key: str, value):
         decky.logger.debug("Executing: set_game_config('%s', '%s')", key, value)
         config.game_config.setSetting(key, value)
-    
-    async def verify_game_exists(self, game_name: str):
-        decky.logger.debug("Executing: verify_game_exists('%s')", game_name)
-        return { "exists": self.ludusavi.check_game(game_name) }
-    
+
     async def backup_game(self, game_name: str):
         decky.logger.debug("Executing: backup_game('%s')", game_name)
         asyncio.create_task(self.ludusavi.backup_game_async(game_name))
 
-
     # Asyncio-compatible long-running code, executed in a task when the plugin is loaded
     async def _main(self):
-        self.ludusavi = Ludusavi(['flatpak run com.github.mtkennerly.ludusavi', 'ludusavi', 'ludusavi.exe'])
+        self.ludusavi = Ludusavi(
+            ["flatpak run com.github.mtkennerly.ludusavi", "ludusavi", "ludusavi.exe"]
+        )
 
     # Function called first during the unload process, utilize this to handle your plugin being removed
     async def _unload(self):
--- py_modules/ludusavi.py
+++ py_modules/ludusavi.py
@@ -12,7 +12,7 @@ class Ludusavi:
     _env: dict[str, str] = None
 
     def __init__(self, bin_paths: list[str]):
-        self.set_env()
+        self._set_env()
 
         for path in bin_paths:
             if self._try_initialize(*path.split(" ")):
@@ -20,34 +20,16 @@ class Ludusavi:
 
         decky.logger.info("Using Binary: %s", self.bin_path)
 
-    def set_env(self):
-        # Fix env for flatpak support.
-        self._env = os.environ.copy()
-
-        if "XDG_RUNTIME_DIR" not in self._env:
-            self._env["XDG_RUNTIME_DIR"] = "/run/user/1000"
-        if "LD_LIBRARY_PATH" in self._env:
-            del self._env["LD_LIBRARY_PATH"]
-
-        decky.logger.debug("Using Environment: %s", self._env)
-
-    def check_game(self, game_name: str):
-        try:
-            # Find if the game 'game_name' has support for backup.
-            cmd = [*self.bin_path, "find", "--api", "--backup", game_name]
-            decky.logger.info("Running command: %s", subprocess.list2cmdline(cmd))
-
-            output = subprocess.check_output(cmd, stderr=subprocess.STDOUT, text=True)
-            data = json.loads(output)
-            return data["games"][game_name] is not None
-        except Exception as e:
-            output = e.output
-            return False
-        finally:
-            decky.logger.debug(output)
-
     async def backup_game_async(self, game_name: str):
-        cmd = [*self.bin_path, "backup", "--api", "--force", game_name]
+        await self._run_command(
+            [*self.bin_path, "backup", "--api", "--force", game_name]
+        )
+    async def restore_game_async(self, game_name: str):
+        await self._run_command(
+            [*self.bin_path, "backup", "--api", "--force", game_name]
+        )
+
+    async def _run_command(self, cmd: list[str], event: str):
         decky.logger.info("Running command: %s", subprocess.list2cmdline(cmd))
 
         try:
@@ -62,14 +44,25 @@ class Ludusavi:
             json_str = stdout.decode()
             decky.logger.debug(json_str)
 
-            json_end = json_str.rindex('}') + 1
+            json_end = json_str.rindex("}") + 1
             json_data = json.loads(json_str[:json_end])
 
-            await decky.emit("backup_game_complete", json_data)
+            await decky.emit(event, json_data)
+
             return json_data
         except Exception as e:
-            decky.logger.error(e)
-            await decky.emit("backup_game_complete", {"errors": {"pluginError": e}})
+            await decky.emit(event, {"errors": {"pluginError": e}})
+
+    def _set_env(self):
+        # Fix env for flatpak support.
+        self._env = os.environ.copy()
+
+        if "XDG_RUNTIME_DIR" not in self._env:
+            self._env["XDG_RUNTIME_DIR"] = "/run/user/1000"
+        if "LD_LIBRARY_PATH" in self._env:
+            del self._env["LD_LIBRARY_PATH"]
+
+        decky.logger.debug("Using Environment: %s", self._env)
 
     def _try_initialize(self, *bin_path: str) -> bool:
         try:
