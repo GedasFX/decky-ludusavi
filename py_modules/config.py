4230da48e0bbb4dc6c27224f2fafb63af95ee2c0
GedasFX <contact@gedas.dev>
2025-03-28 00:23:36 +0200

    revert dir change, pointless

--- py_modules/config.py
+++ py_modules/config.py
@@ -1,13 +1,11 @@
 import decky  # type: ignore
 from settings import SettingsManager  # type: ignore
-import shutil
-import os
 
 app_config = SettingsManager(
     name="settings", settings_directory=decky.DECKY_PLUGIN_SETTINGS_DIR
 )
 game_config = SettingsManager(
-    name="games", settings_directory=decky.DECKY_PLUGIN_RUNTIME_DIR
+    name="games", settings_directory=decky.DECKY_PLUGIN_SETTINGS_DIR
 )
 
 
@@ -20,11 +18,4 @@ def migrate():
 
 def _migrate_v1():
     app_config.setSetting("auto_backup_toast_enabled", True)
-
-    if os.path.isfile(decky.DECKY_PLUGIN_SETTINGS_DIR + "/games.json"):
-        shutil.move(
-            decky.DECKY_PLUGIN_SETTINGS_DIR + "/games.json",
-            decky.DECKY_PLUGIN_RUNTIME_DIR + "/games.json",
-        )
-
     app_config.setSetting("version", 1)
