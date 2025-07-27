bb3d6a9263422ee12fff38d709fc0911e0b0dbc5
Snappi <contact@gedas.dev>
2025-07-27 21:05:32 +0300

    Add Logging for app_id

--- src/util/steamUtil.ts
+++ src/util/steamUtil.ts
@@ -1,19 +1,21 @@
-import { getGameNameFormManifest } from "./backend"
+import { getGameNameFormManifest, log } from "./backend";
 
 declare const appStore: any;
 export const resolveGameName = async (appId: number) => {
-    const [launchOptions, appOverview, manifest] = await Promise.all([
-        SteamClient.Apps.GetLaunchOptionsForApp(appId),
-        appStore.GetAppOverviewByAppID(appId),
-        getGameNameFormManifest(appId.toString())
-    ]);
+  const [launchOptions, appOverview, manifest] = await Promise.all([
+    SteamClient.Apps.GetLaunchOptionsForApp(appId),
+    appStore.GetAppOverviewByAppID(appId),
+    getGameNameFormManifest(appId.toString()),
+  ]);
 
-    const manifestName = manifest.games ? Object.keys(manifest.games)[0] : ''
+  const manifestName = manifest.games ? Object.keys(manifest.games)[0] : "";
+  const steamGameName = launchOptions[0].strGameName;
+  const nonSteamGameName = appOverview?.display_name;
 
-    console.log('manifestName', manifest, manifestName)
+  await log(
+    `APP_ID ${appId}: Manifest - [${manifestName ?? ''}], Launch Options - [${launchOptions[0].strGameName ?? ''}], App Overview - [${appOverview?.display_name ?? ''}].`,
+    "info"
+  );
 
-    const steamGameName = manifestName ?? launchOptions?.strGameName;
-    const nonSteamGameName = appOverview?.display_name;
-
-    return steamGameName ?? nonSteamGameName;
-}
+  return manifestName ?? steamGameName ?? nonSteamGameName;
+};
